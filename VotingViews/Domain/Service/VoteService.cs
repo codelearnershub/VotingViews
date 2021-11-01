1fda0560dec60d14246a4ca3b49a1ba984266eae
lutyoung <ghazallutfulmannan@gmail.com>
2021-11-01 13:28:04 +0100

    Changes on Position And Mostly on ElectionService

---
+++ VotingViews/Domain/Service/VoteService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingViews.Domain.Service
+{
+    public class VoteService
+    {
+        public Task VotedContestant()
+        {
+            return null;
+        }
+
+        public  Task Vote()
+        {
+            return null;
+        }
+
+        private bool HasVotedBefore()
+        {
+           return true;
+        }
+    }
+}
