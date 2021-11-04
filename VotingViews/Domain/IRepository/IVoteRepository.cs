71a306e7e4b670b35b1e59f9080a60c3c682abff
lutyoung <ghazallutfulmannan@gmail.com>
2021-11-04 17:48:34 +0100

    vvv

---
+++ VotingViews/Domain/IRepository/IVoteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VotingViews.Model.Entity;
+
+namespace VotingViews.Domain.IRepository
+{
+    public interface IVoteRepository
+    {
+        public IQueryable<Vote> Query();
+
+        public Vote FindVoteById(int id);
+
+        public Vote GetVoteByPositionId(int id);
+    }
+}
